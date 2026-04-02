3280988b14dc330d967d3df4dea7196fb2224f12
alexandre-a4 <alexandre.lamulle@gmail.com>
2026-04-02 11:36:27 +0200

    Update rfid.ts

--- rfid.ts
+++ rfid.ts
@@ -12,18 +12,22 @@ namespace RFID {
     //% weight=100
     export function init(rx: SerialPin, tx: SerialPin): void {
         serial.redirect(tx, rx, BaudRate.BaudRate9600)
+        serial.setRxBufferSize(64)
     }
 
     /**
-     * Lire l'ID RFID
+     * Lire l'ID RFID (formaté 10 chiffres)
      */
     //% block="lire ID RFID"
     //% weight=90
     export function readID(): string {
         let data = serial.readString()
+        data = data.replace(/\r|\n/g, "") // enlever retour chariot/ligne
 
-        if (data.length >= 10) {
-            lastID = data.substr(0, 10)
+        // Chercher un ID sur 10 chiffres dans la trame
+        let match = /[0-9]{10}/.exec(data)
+        if (match) {
+            lastID = match[0]
         }
 
         return lastID
