a351de9acebb01b883054a261474decb2089cbbc
alexandre-a4 <alexandre.lamulle@gmail.com>
2026-04-02 14:07:39 +0200

    Update rfid.ts

--- rfid.ts
+++ rfid.ts
@@ -1,6 +1,19 @@
 //% weight=100 color=#0fbc11 icon="\uf2db"
 namespace RFID {
 
+    function hexToDec(hex: string): number {
+        let result = 0
+        let digits = "0123456789ABCDEF"
+
+        for (let i = 0; i < hex.length; i++) {
+            let c = hex.charAt(i)
+            let value = digits.indexOf(c)
+            result = result * 16 + value
+        }
+
+        return result
+    }
+
     //% block="initialiser RFID RX %rx TX %tx"
     export function init(rx: SerialPin, tx: SerialPin): void {
         serial.redirect(tx, rx, BaudRate.BaudRate9600)
@@ -11,9 +24,8 @@ namespace RFID {
         let raw = serial.readString()
 
         if (raw.length >= 10) {
-            // extraire partie utile (8 caractères après "66")
             let hexStr = raw.substr(2, 8)
-            return parseInt(hexStr, 16)
+            return hexToDec(hexStr)
         }
 
         return 0
